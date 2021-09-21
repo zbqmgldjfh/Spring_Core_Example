5a71ed2437323515fd998db5586de045362fc06a
zbqmgldjfh <zbqmgldjfh@naver.com>
2021-09-21 16:46:39 +0900

    인터페이스 InitializingBean, DisposableBean

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,44 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    // 의존관계 주입이 끝난다면
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈 종료시
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+}
